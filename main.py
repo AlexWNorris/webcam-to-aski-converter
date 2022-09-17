ad6447d114f49c190f7cbd58333c23c5d991c9b8
Alex Norris <81189538+AlexWNorris@users.noreply.github.com>
2022-09-17 14:36:41 +0100

    Make display window larger

--- main.py
+++ main.py
@@ -1,47 +1,57 @@
-
+from time import sleep
 import cv2
 from numpy import round, uint8 ,zeros
 import numpy
 
 #ascii darkness list
-ascii='Ñ@#W$9876543210?!abc;:+=-,._'
+ascii='Ñ@#W$9876543210?!abc;:+=-,._ '
 indexShift=255/len(ascii)
 
-cv2.namedWindow('webcam')
-cv2.namedWindow("asciiWindow")
-
-wc=cv2.VideoCapture(0)
-
+#ranges from 0-->1
+pixelationConstant=0.02
 pw,ph=64,64
-w,h=512,512
+w,h=1920,1080
+pws=int(w*pixelationConstant)
+phs=int(h*pixelationConstant)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 org = (0, 0)
-fontScale = 0.22
+fontScale = 0.4
 color = (255, 255, 255)
 thickness = 1
+
+wc=cv2.VideoCapture(0)
+codec = 0x47504A4D  # MJPG
+wc.set(cv2.CAP_PROP_FPS, 60.0)
+wc.set(cv2.CAP_PROP_FOURCC, codec)
+wc.set(cv2.CAP_PROP_FRAME_WIDTH, w)
+wc.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
+
 if wc.isOpened():
     rval,frame=wc.read()
+    frame=cv2.flip(frame,1)
 else:
     rval = False
-
+#0.5625
+scaler=1/1.5
+sw,sh=int(w*scaler),int(h*scaler)
 while rval:
     blackImage=zeros((w,h,3),numpy.uint8)
     asciiImageSymbols=[]
     resized=cv2.resize(frame,(pw,ph),interpolation=cv2.INTER_LINEAR)
-    pixelation=cv2.resize(resized,(w,h),interpolation=cv2.INTER_NEAREST)
+    pixelation=cv2.resize(resized,(sw,sh),interpolation=cv2.INTER_NEAREST)
     grayscalePixalation=cv2.cvtColor(pixelation,cv2.COLOR_BGR2GRAY)
     image=''
-    for i in range(int(pw)):
+    for i in range(int(ph)):
         temp=''
-        for x in range(int(ph)):
-            samplePixel=grayscalePixalation[(i*int((w/pw)))][(x*int((h/ph)))]
+        for x in range(int(pw)):
+            samplePixel=grayscalePixalation[(i*int(len(grayscalePixalation)/pw))][int(x*(len(grayscalePixalation[0])/ph))]
             character=ascii[len(ascii)-int(round(samplePixel/indexShift,0))-1]
-            cv2.putText(blackImage,character,(int((x+1)*(w/pw)),int(h/ph*(i+0.5))), font,fontScale,color, thickness, cv2.LINE_AA)
-    cv2.imshow("webcam",frame)
-    cv2.imshow("asciiWindow",blackImage)
+            cv2.putText(blackImage,character,(int(x*(w/pw)/(1/scaler)),int((i-1)*(h/ph)/(1/scaler))), font,fontScale,color, thickness, cv2.LINE_AA)
+    cv2.imshow("Window",blackImage)
     rval,frame=wc.read()
+    frame=cv2.flip(frame,1)
     key = cv2.waitKey(20)
     if key == 27:
         break
-wc.release()
+cv2.destroyAllWindows()
